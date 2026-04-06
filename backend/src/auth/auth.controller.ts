12d7b8d331531f620dcdb5783cf559b771dd9aec
Viktoria Muratova <anna.markuna@gmail.com>
2026-04-06 19:40:04 +0300

    задействовала dto

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-  @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
-    //   const user = await this.authService.register(body.email, body.password);
-    //   return { message: 'Пользователь зарегистрирован', userId: user.id };
-  }
+  // constructor(private authService: AuthService) {}
+  // @Post('register')
+  // async register(@Body() createUserDto: CreateUserDto) {
+  //   const user = await this.authService.register(
+  //     createUserDto.email,
+  //     createUserDto.password,
+  //   );
+  //   return { message: 'Registration successful', userId: user.id };
+  // }
 }
