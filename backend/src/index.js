61ee812b5d60f16bf3dae7eb38cae400cc5df3f9
Viktoria Muratova <anna.markuna@gmail.com>
2026-04-10 06:10:58 +0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,5 +46,5 @@ app.get("/users", async (req, res) => {
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log('Server running on port ${PORT}');
 });
