3a70a8f80aae435ac9c19b039ae41c6ebea0a97b
Viktoria Muratova <anna.markuna@gmail.com>
2026-04-22 18:23:06 +0300

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Конфигурация Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Название API')
+    .setDescription('Описание API')
+    .setVersion('1.0')
+    .addTag('users') // Опционально
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // 'api/docs' - путь к Swagger UI
+
+  await app.listen(3000);
 }
-bootstrap().catch((error) => console.error(error));
+bootstrap();
