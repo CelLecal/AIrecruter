b90a3a07070b2bc4d34f4b3814f06c6786697dfb
Viktoria Muratova <anna.markuna@gmail.com>
2026-04-29 16:39:53 +0300

    поправила main и package

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
 import * as yaml from 'yaml';
 import { join } from 'path';
-import { setGlobalProxyFromEnv } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
