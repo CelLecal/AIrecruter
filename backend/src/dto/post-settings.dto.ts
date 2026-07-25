c95d9114a589ad6bc5a97bf009ebd55b9996638c
Viktoria Muratova <anna.markuna@gmail.com>
2026-07-25 05:17:50 +0300

    добавляю запуск

--- backend/src/dto/post-settings.dto.ts
+++ backend/src/dto/post-settings.dto.ts
@@ -1,10 +1,6 @@
 // activate.dto.ts
-import { IsString, IsNotEmpty, IsBoolean } from "class-validator";
 
 export class PostSettingsDto {
-  @IsString()
-  @IsNotEmpty()
   id!: number;
-  @IsBoolean()
   is_active!: boolean;
 }
